48aed50d3458748a3df91db9f6346640f66cebe7
vatdaell <ansafahmad70@gmail.com>
2024-03-20 19:16:34 -0400

    TEST calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import NavigationBar from "./NavigationBar/NavigationBar"
 import TabbedHero from "./TabbedHero/TabbedHero"
 
+
 function App() {
   console.log(import.meta.env.VITE_TEST)
+
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", import.meta.env.VITE_AUTHORIZATION);
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    redirect: "follow"
+  };
+
+  fetch(import.meta.env.VITE_API_URL, requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
 
 
   return (
